9bc9a2a45fbbbde862290c7968f2a9ab81e4ce75
AFTBayat <47597979+AFTBayat@users.noreply.github.com>
2019-06-24 13:41:48 +0430

    1

--- 1.py
+++ 1.py
@@ -1,9 +1,12 @@
 import pandas as pd
-
+#%matplotlib inline
+import matplotlib.pyplot as plt
 df = pd.read_excel (r'E:\company\python\Oil_well_acidizing\DataSet\02_preprocessing_after_normalizing_values.xlsx') #for an earlier version of Excel, you may need to use the file extension of 'xls'
 #print (df)
 df.head()
-df.info()
-df.describe()
+print(df.info())
+print(df.describe())
 df["Level"].value_counts()
+df.hist(bins=5,figsize=(20,15))
+plt.show()
 

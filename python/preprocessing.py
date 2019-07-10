407fadbffe60e39df4511389eac7d3f7a3c9c94d
AFTBayat <47597979+AFTBayat@users.noreply.github.com>
2019-07-10 17:25:24 +0430

    11

--- python/preprocessing.py
+++ python/preprocessing.py
@@ -2,7 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-os.chdir("../Dataset/Second dataset")
+os.chdir("../Dataset/Second Dataset")
 import pandas as pd
 from pandas.plotting import scatter_matrix
 from sklearn.decomposition import PCA
@@ -46,7 +46,7 @@ def featureCorrelationRanking(df):
     s = c.unstack()
     so = s.sort_values(kind="quicksort")
     so = pd.DataFrame(so.dropna())
-    so.to_excel("feture_correlation_ranking.xlsx")
+    so.to_excel("feture_correlation_ranking2.xlsx")
     return so
 
 def PCA_(X, n_components):
@@ -66,7 +66,7 @@ def save_box_plot(df1, df2):
         plt.boxplot(arr1[:, i])
         plt.subplot(122)
         plt.boxplot(arr2[:, i])
-        fig.savefig(str('../figures/fig' + str(i) + '.jpg'))
+        fig.savefig(str('../figures2/fig' + str(i) + '.jpg'))
         plt.show(block=False)
         plt.pause(0.1)
         plt.close()
@@ -81,9 +81,8 @@ def plot_2d_features(df):
     
     df = df.drop(['Id', 'Level', 'Column Volume (ft3)'], axis=1)
     for col in df.columns:
-        print(col)
         target = df[col]
-        tmp_df = df#df.drop([col], axis = 1)
+        tmp_df = df.drop([col], axis = 1)
         pca = PCA(n_components=2)
         tmp_df = pca.fit_transform(tmp_df)
         principalDf = pd.DataFrame(data = tmp_df
@@ -102,7 +101,7 @@ def plot_2d_features(df):
         sns.scatterplot(principalDf['pc1'], principalDf['pc2'], hue = principalDf[str(col)])
         #plt.title('Carbonate_sanstone wells based on its location')
         plt.legend()
-        plt.savefig('2D_'+str(col))
+        #plt.savefig('2D_'+str(col))
         plt.show()
 
 def number_of_optimal_k_means_classes(df):
@@ -179,58 +178,58 @@ def evaluate_pca_effect(df):
     
 
 #____________________________________________________________ Step One ___________________________________________
-file_name = 'Sample Synthetic Data - Revised.xlsx'
-df = pd.read_excel(file_name, index_col=0, index=False)
-cols=["Oil/Gas","Open/Cased","Water/Oil0Based","Loss/Gain","Carbonate/Sandstone","Wettability"]
-#for col in cols:
- #   enc_df[col]=labelEncoder(df,"col")
-
+#file_name = 'Sample Synthetic Data - Revised.xlsx'
+#df = pd.read_excel(file_name)
 
 #____________________________________________________________ Step Two ___________________________________________
-enc_df = labelEncoder(df,cols)
-print(enc_df.iloc[0])
+
+#cols = ["Oil/Gas","Open/Cased","Water/Oil0Based","Loss/Gain","Carbonate/Sandstone","Wettability" ]
+#for col in cols:
+#    enc_df = labelEncoder(df, col)
+
+#enc_df = labelEncoder(df, cols)
+#print(enc_df.iloc[0])
 #____________________________________________________________ Step Three ___________________________________________
-norm_df = normalizeValues(df)
-input(norm_df.iloc[0])
+#norm_df = normalizeValues(df)
+#input(norm_df.iloc[0])
 #____________________________________________________________ Step Four ___________________________________________
-save_box_plot(enc_df, norm_df)
+#save_box_plot(enc_df, norm_df)
 
 #____________________________________________________________ Step Five ___________________________________________
-plot_corr(enc_df,size=10)
-
+df = pd.read_excel("022_preprocessing_after_normalizing_values.xlsx")
+plot_corr(df,size=10)
 #____________________________________________________________ Step Six ___________________________________________
 
 #file_name = '02_preprocessing_after_normalizing_values.xlsx'
 #df = pd.read_excel(file_name, index = True)
 
-plot_corr(df, 5)
 
 #plot_2d_features(df)
 
 
-number_of_optimal_k_means_classes(df)
-k_means(df)
+#number_of_optimal_k_means_classes(df)
+##k_means(df)
 
-er = evaluate_pca_effect(df)
+#er = evaluate_pca_effect(df)
 
 #____________________________________________________________ Step Seven ___________________________________________
-draw_pair_wise_scatter_plots(enc_df)
+#draw_pair_wise_scatter_plots(enc_df)
 
 #____________________________________________________________ Step Eight ___________________________________________
 
 
 
-pca_df = pd.DataFrame(df)#PCA(df, 2))
+#pca_df = pd.DataFrame(df)#PCA(df, 2))
 
 
 
 
 
-pca_df.plot(style=['o','rx'])
+#pca_df.plot(style=['o','rx'])
 
-plt.show()
+#plt.show()
 
-scatter_matrix(pca_df, alpha=0.2)
+#scatter_matrix(pca_df, alpha=0.2)
 
 
 

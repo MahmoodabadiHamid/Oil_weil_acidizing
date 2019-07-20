c11cca3babdd4ee8a053fa6e9741e87bc20c3011
Hamid Mahmoodabadi <newwink@yahoo.com>
2019-07-20 18:23:13 +0430

    non-linear correlation matrix

--- python/mutually information.py
+++ python/mutually information.py
@@ -5,20 +5,7 @@ import seaborn as sns
 import os
 
 
-os.chdir("../Dataset/Second dataset")
 
-file_name = '022_preprocessing_after_normalizing_values.xlsx'
-df = pd.read_excel(file_name, index = True)
-
-sns.heatmap(
-    data=df.corr(),
-    annot=True,
-    fmt='.2f',
-    cmap='RdYlGn'
-)
-
-fig = plt.gcf()
-fig.set_size_inches(20, 16)
 
 def calc_MI(X,Y,bins):
 
@@ -38,16 +25,37 @@ def shan_entropy(c):
     H = -sum(c_normalized* np.log2(c_normalized))  
     return H
 
-  
-for i in range(1,26):
-   for j in range(1,26):
-     print(i,j,calc_MI(df.iloc[:,-i],df.iloc[:,-j],500))
 
-       
+def main():
+   from sklearn import preprocessing
+   os.chdir("../Dataset/Second dataset")
+   file_name = '022_preprocessing_after_normalizing_values.xlsx'
+   df = pd.read_excel(file_name, index = True)
+
+
+   fig = plt.gcf()
+   fig.set_size_inches(20, 16)
+   mutual_matrix = np.zeros([df.shape[1],df.shape[1]])
+   for i in range(df.shape[1]):
+      for j in range(df.shape[1]):
+        mutual_matrix[i, j] = calc_MI(df.iloc[:,-i],df.iloc[:,-j],500)
+
+   min_max_scaler = preprocessing.MinMaxScaler()
+   mutual_matrix = min_max_scaler.fit_transform(mutual_matrix)
    
-
-   
-
+   pl, ax = plt.subplots(figsize=(20, 15))
+   hm = sns.heatmap(np.round(mutual_matrix,2), annot=True, ax=ax, cmap="coolwarm",fmt='.2f',
+                     linewidths=.05, annot_kws={"size": 7})
+   hm.set_xticklabels(df.columns, rotation = 75, fontsize = 8)
+   hm.set_yticklabels(df.columns, rotation = 30, fontsize =11)
+   pl.subplots_adjust(top=0.93, left=0.15, bottom = 0.2)
+   t= pl.suptitle('Well Attributes non-linear Correlation Heatmap', fontsize=14)
+   pl.savefig('non_linear_Correlation_Plot.jpg')
+   plt.show()
+    
+ 
+if __name__ == '__main__':
+    main()
 
 
 

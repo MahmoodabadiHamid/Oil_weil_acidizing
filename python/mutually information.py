2ba453c2ab3d063471ce413750c5575052fc5eba
Hamid Mahmoodabadi <newwink@yahoo.com>
2019-07-25 16:44:44 +0430

    non-linear correlation excel added

--- python/mutually information.py
+++ python/mutually information.py
@@ -4,11 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-
-
-
 def calc_MI(X,Y,bins):
-
    c_XY = np.histogram2d(X,Y,bins)[0]
    c_X = np.histogram(X,bins)[0]
    c_Y = np.histogram(Y,bins)[0]
@@ -19,29 +15,39 @@ def calc_MI(X,Y,bins):
 
    MI = H_X + H_Y - H_XY
    return MI
+
 def shan_entropy(c):
     c_normalized = c / float(np.sum(c))
     c_normalized = c_normalized[np.nonzero(c_normalized)]
     H = -sum(c_normalized* np.log2(c_normalized))  
     return H
 
-
 def main():
    from sklearn import preprocessing
    os.chdir("../Dataset/Second dataset")
    file_name = '022_preprocessing_after_normalizing_values.xlsx'
    df = pd.read_excel(file_name, index = True)
-
-
+   corr_df = pd.DataFrame({"First Feature":[], "Second Feature":[], "Correlation":[]})
+   df = df.drop(['Id', 'Level'], axis=1)
    fig = plt.gcf()
    fig.set_size_inches(20, 16)
    mutual_matrix = np.zeros([df.shape[1],df.shape[1]])
    for i in range(df.shape[1]):
       for j in range(df.shape[1]):
-        mutual_matrix[i, j] = calc_MI(df.iloc[:,-i],df.iloc[:,-j],500)
+         mutual_matrix[i, j] = calc_MI(df.iloc[:,-i],df.iloc[:,-j],500)
+         
+         corr_df = corr_df.append({
+            "First Feature":[(df.columns[i])],
+            "Second Feature":[(df.columns[j])],
+            "Correlation" : round(mutual_matrix[i, j],2)
+                    }, ignore_index = True)
+      
 
    min_max_scaler = preprocessing.MinMaxScaler()
    mutual_matrix = min_max_scaler.fit_transform(mutual_matrix)
+   
+   corr_df[["Correlation"]] = min_max_scaler.fit_transform(corr_df[["Correlation"]])
+   corr_df.to_excel("second_dataset_non_linear_correlation_ranking.xlsx")
    
    pl, ax = plt.subplots(figsize=(20, 15))
    hm = sns.heatmap(np.round(mutual_matrix,2), annot=True, ax=ax, cmap="coolwarm",fmt='.2f',
@@ -51,7 +57,7 @@ def main():
    pl.subplots_adjust(top=0.93, left=0.15, bottom = 0.2)
    t= pl.suptitle('Well Attributes non-linear Correlation Heatmap', fontsize=14)
    pl.savefig('non_linear_Correlation_Plot.jpg')
-   plt.show()
+   #plt.show()
     
  
 if __name__ == '__main__':
